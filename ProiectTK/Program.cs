ed0d58e786f675680923cf5628451c150b56008f
Gheorghe Luța <georgelutaoff@gmail.com>
2023-11-08 18:35:17 +0200

    tema practica 2

---
+++ ProiectTK/Program.cs
@@ -0,0 +1,175 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+using System;
+using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace ProiectTK
+{
+    class Program : GameWindow
+    {
+
+
+        private int xMoved = 0;
+        private int yMoved = 0;
+
+
+        public Program() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            Console.WriteLine("OpenGl versiunea: " + GL.GetString(StringName.Version));
+            Title = "OpenGl versiunea: " + GL.GetString(StringName.Version) + " (mod imediat)";
+
+        }
+
+        /**Setare mediu OpenGL și încarcarea resurselor (dacă e necesar) - de exemplu culoarea de
+           fundal a ferestrei 3D.
+           Atenție! Acest cod se execută înainte de desenarea efectivă a scenei 3D. */
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.Blue);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        /**Inițierea afișării și setarea viewport-ului grafic. Metoda este invocată la redimensionarea
+           ferestrei. Va fi invocată o dată și imediat după metoda ONLOAD()!
+           Viewport-ul va fi dimensionat conform mărimii ferestrei active (cele 2 obiecte pot avea și mărimi 
+           diferite). */
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(30, 30, 30, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+
+        }
+
+        /** Secțiunea pentru "game logic"/"business logic". Tot ce se execută în această secțiune va fi randat
+            automat pe ecran în pasul următor - control utilizator, actualizarea poziției obiectelor, etc. */
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState keyboard = Keyboard.GetState();
+            MouseState mouse = Mouse.GetState();
+
+            if (keyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            if (keyboard[Key.Left] || keyboard[Key.A])
+            {
+                xMoved--;
+            }
+
+            if (keyboard[Key.Right] || keyboard[Key.D])
+            {
+                xMoved++;
+            }
+
+            if (keyboard[Key.Up] || keyboard[Key.W])
+            {
+                yMoved++;
+            }
+
+            if (keyboard[Key.Down] || keyboard[Key.S])
+            {
+                yMoved--;
+            }
+
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                xMoved++;
+                yMoved--;
+            }
+
+            if (mouse.RightButton == ButtonState.Pressed)
+            {
+                xMoved--;
+                yMoved++;
+            }
+        }
+
+        /** Secțiunea pentru randarea scenei 3D. Controlată de modulul logic din metoda ONUPDATEFRAME().
+            Parametrul de intrare "e" conține informatii de timing pentru randare. */
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            DrawAxes();
+
+            DrawObjects();
+
+
+
+            // Se lucrează în modul DOUBLE BUFFERED - câtă vreme se afișează o imagine randată, o alta se randează în background apoi cele 2 sunt schimbate...
+            SwapBuffers();
+        }
+
+        private void DrawAxes()
+        {
+
+            //GL.LineWidth(3.0f);
+
+            GL.Begin(PrimitiveType.Polygon);
+
+            GL.Color3(Color.Red);
+            GL.Vertex3(0 + xMoved, 0 + yMoved, 0);
+            GL.Vertex3(0 + xMoved, 10 + yMoved, 0);
+            GL.Vertex3(10 + xMoved, 10 + yMoved, 0);
+            GL.Vertex3(10 + xMoved, 0 + yMoved, 0);
+
+            GL.End();
+
+
+        }
+
+        private void DrawObjects()
+        {}
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            /**Utilizarea cuvântului-cheie "using" va permite dealocarea memoriei o dată ce obiectul nu mai este
+               în uz (vezi metoda "Dispose()").
+               Metoda "Run()" specifică cerința noastră de a avea 30 de evenimente de tip UpdateFrame per secundă
+               și un număr nelimitat de evenimente de tip randare 3D per secundă (maximul suportat de subsistemul
+               grafic). Asta nu înseamnă că vor primi garantat respectivele valori!!!
+               Ideal ar fi ca după fiecare UpdateFrame să avem si un RenderFrame astfel încât toate obiectele generate
+               în scena 3D să fie actualizate fără pierderi (desincronizări între logica aplicației și imaginea randată
+               în final pe ecran). */
+            using (Program example = new Program())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+
+}
